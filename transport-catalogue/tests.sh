edc2ebc44992d54bbd711d8c658373c6a9170e1a
Oleg Polivets <jsbot@ya.ru>
2022-09-18 02:11:45 +0300

    sprin12: tests

--- transport-catalogue/tests.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-mkdir build-tests 
-cd $_ 
-cmake -DCMAKE_BUILD_TYPE=Debug .. 
-make -j4 && ./sprint9 < ../tsC_case1_input.txt > out.txt && diff -s -w ../tsC_case1_output1.txt out.txt
-cd .. && rm -rf build-tests
